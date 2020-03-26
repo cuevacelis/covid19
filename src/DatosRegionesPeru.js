44b352211cc53a44322b07405ec0bc4dc432bdc8
cuevacelis <cuevacelis@hotmail.com>
2020-03-26 16:57:35 -0500

    Quite la img de Reportes Diarios

--- src/DatosRegionesPeru.js
+++ src/DatosRegionesPeru.js
@@ -2,15 +2,6 @@ import React from 'react';
 function DatosRegionesPeru() {
     return (
         <div className="container">
-            <section className="DatosRegionesPeru jumbotron text-center">
-                <div className=" text-center">
-                    <h2 className="display-4">Reporte Diario</h2>
-                    <figure className="figure text-center">
-                        <img src="https://pbs.twimg.com/media/ETkznp4XsAAcPa1?format=jpg&name=large" className="figure-img img-fluid rounded" alt="Reporte de Coronovirus"/>
-                        <figcaption className="figure-caption">Reporte Diario del Covid-19 en las regiones del Perú.</figcaption>
-                    </figure>
-                </div>
-            </section>
 
             <hr></hr>
 
