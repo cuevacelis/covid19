4faa87922abd3b2621a3d01e2103197d8a716ec2
cuevacelis <cuevacelis@hotmail.com>
2020-03-29 22:40:53 -0500

    Correccion de errores de la consola

--- src/DatosCountry.js
+++ src/DatosCountry.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,prevState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 //alert("1")
 function DatosCountry() {
@@ -42,13 +42,13 @@ function DatosCountry() {
 
                     <div className="container form-group">
                         <label className="display-4">Selecciona tu País :</label>
-                        <select id="select_country" data-live-search="true" data-show-subtext="true" className="form-control" onChange={e => setIndexDatosCountry(e.currentTarget.value)}>
+                        <select id="select_country" value={indexDatosCountry} data-live-search="true" data-show-subtext="true" className="form-control" onChange={e => setIndexDatosCountry(e.currentTarget.value)}>
                             {
                                 loadingCounrty ?
                                     <option>Obteniendo datos ...</option>
                                 :
                                 datosCountry.map((datoCountry,index) => (
-                                    <option key={index} selected={index==indexDatosCountry} value={index}>{datoCountry.country +" - "+ "Top: "+(index+1)}</option>
+                                    <option key={index} value={index}>{datoCountry.country +" - "+ "Top: "+(index+1)}</option>
                                     )
                                 )
                             }
